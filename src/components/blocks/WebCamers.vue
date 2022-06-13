33bc3982e50dd9704478ab173bb8c7e702a3088e
amalthea-code <amalthea22@yandex.ru>
2022-06-13 11:55:06 +0400

    feat: added new fix

--- src/components/blocks/WebCamers.vue
+++ src/components/blocks/WebCamers.vue
@@ -4,13 +4,13 @@
 
     <div class="camers__box">
       <div class="camers__items">
-        <div v-show="select === 0" class="camers__item">
+        <div v-if="select === 0" class="camers__item">
           <iframe src="https://ipeye.ru/ipeye_service/api/iframe.php?iframe_player=1&dev=e851fb8225f04010afd0e642be6e7e09&autoplay=0&archive=1" width="100%" height="600" frameBorder="0" seamless="seamless" allowfullscreen>Ваш браузер не поддерживает фреймы!</iframe>
         </div>
-        <div v-show="select === 1" class="camers__item">
+        <div v-if="select === 1" class="camers__item">
           <iframe src="https://ipeye.ru/ipeye_service/api/iframe.php?iframe_player=1&dev=6209f86bf2bd41c18a1b28ee0bfd2bfe&autoplay=0&archive=1" width="100%" height="600" frameBorder="0" seamless="seamless" allowfullscreen>Ваш браузер не поддерживает фреймы!</iframe>
         </div>
-        <div v-show="select === 2" class="camers__item">
+        <div v-if="select === 2" class="camers__item">
           <iframe src="https://ipeye.ru/ipeye_service/api/iframe.php?iframe_player=1&dev=53ff6fd49e204cc28db2dea0ef638064&autoplay=0&archive=1" width="100%" height="600" frameBorder="0" seamless="seamless" allowfullscreen>Ваш браузер не поддерживает фреймы!</iframe>
         </div>
       </div>
