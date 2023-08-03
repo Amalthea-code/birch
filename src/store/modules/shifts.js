1a8f6f8a05919525da8fd61419da030f4ba005d8
Ruslan Ragilo <ruslanragilo7@gmail.com>
2023-08-03 19:16:19 +0300

    fix

--- src/store/modules/shifts.js
+++ src/store/modules/shifts.js
@@ -1,12 +1,16 @@
 export default {
   namespaced: true,
   state: {
-    shifts: []
+    shifts: [],
+    shiftsPre: []
   },
   mutations: {
     SET_SHIFTS(state, payload) {
       state.shifts = payload;
-    }
+    },
+    SET_SHIFTS_PRE(state, payload) {
+      state.shiftsPre = payload;
+    },
   },
   actions: {
     async fetchShifts ({ commit }) {
@@ -20,12 +24,37 @@ export default {
       }).then((data) => {
         console.log(data.data);
         commit('SET_SHIFTS', data.data)
+        const obj1 = {
+          id: 7,
+          attributes : {
+            ...data.data[data.data.length - 1].attributes
+          }
+        }
+        obj1.attributes.price = 11000
+        delete obj1.attributes.service_name
+        
+        // delete obj1.attributes.surcharge
+        const obj2 = {
+          id: 8,
+          attributes : {
+            ...data.data[data.data.length - 1].attributes
+          }
+        }
+        delete obj2.attributes.service_name
+        delete obj2.attributes.prepaid
+        obj2.attributes.price = 11000
+        const dataPre = [...data.data, obj1, obj2]
+        commit('SET_SHIFTS_PRE', dataPre)
       });
     }
   },
   getters: {
     GET_SHIFTS: (state) => {
       return state.shifts;
+    },
+    GET_SHIFTS_PRE: (state) => {
+      console.log(state.shiftsPre);
+      return state.shiftsPre
     }
   }
 };
